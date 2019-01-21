004866e88ecd7fc2fa992ba56ef0bc6c65d6ab3a
cxxtreasure <cxx_treasure@163.com>
2019-01-21 20:42:04 +0800

    ApplicationContext、xml bean配置、三种对象的创建方式

--- spring_dayc01/src/cn/itcast/b_create/Demo.java
+++ spring_dayc01/src/cn/itcast/b_create/Demo.java
@@ -7,17 +7,17 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 import cn.itcast.bean.User;
 
 public class Demo {
-	@Test
+	//@Test
 	// 创建方式1:空参构造
-	public void fun1() {
+	//public void fun1() {
 		// 1 创建容器对象
-		ApplicationContext ac = new ClassPathXmlApplicationContext("cn/itcast/b_create/applicationContext.xml");
+//		ApplicationContext ac = new ClassPathXmlApplicationContext("cn/itcast/b_create/applicationContext.xml");
 		// 2 向容器"要"user对象
-		User u = (User) ac.getBean("user");
+		//User u = (User) ac.getBean("user");
 
 		// 3 打印user对象
-		System.out.println(u);
-	}
+		//System.out.println(u);
+	//}
 
 	// 创建方式2:静态工厂
 //	@Test
--- spring_dayc01/src/cn/itcast/bean/User.java
+++ spring_dayc01/src/cn/itcast/bean/User.java
@@ -1,8 +1,32 @@
 package cn.itcast.bean;
 
 public class User {
+	private Car car;
 	public User() {
 		System.out.println("User对象空参构造方法!!!!");
+	}
+	public User(String name, Car car) {
+		System.out.println("User(String name, Car car)!!");
+		this.name = name;
+		this.car = car;
+	}
+	
+	public User(Car car,String name) {
+		System.out.println("User(Car car,String name)!!");
+		this.name = name;
+		this.car = car;
+	}
+	
+	public User(Integer name, Car car) {
+		System.out.println("User(Integer name, Car car)!!");
+		this.name = name+"";
+		this.car = car;
+	}
+	public Car getCar() {
+		return car;
+	}
+	public void setCar(Car car) {
+		this.car = car;
 	}
 	private String name;
 	private Integer age;
